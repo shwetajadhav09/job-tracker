00491d492611d8bf9a328fa9e5a22a1d91e65f9f
Shweta Jadhav <shwetajadhav99255@gmail.com>
2024-09-24 11:59:50 +0530

    updated

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://job-tracker-tau-flax.vercel.app/",
+  baseURL: "http://localhost:8080",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
